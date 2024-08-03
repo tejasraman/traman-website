29ab8c7c4abc6d64003e22a3491fb968261951c5
Tejas Raman <tejasraman@icloud.com>
2024-08-02 21:15:39 -0600

    Add email, GH

---
+++ scripts.js
@@ -0,0 +1,17 @@
+function hovergh(element, fn) {
+    element.setAttribute('src', 'assets/gh-hovered.svg');
+}
+
+function unhovergh(element, fn) {
+    element.setAttribute('src', 'assets/gh.svg');
+}
+
+
+function hoverml(element, fn) {
+    element.setAttribute('src', 'assets/email-hovered.svg');
+}
+
+function unhoverml(element, fn) {
+    element.setAttribute('src', 'assets/email.svg');
+}
+
